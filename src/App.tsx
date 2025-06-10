fb73bf4812f1db70153e50a7611639c3d237a70b
Aizaz Ahmad <aizazahmed476@gmail.com>
2025-06-09 22:09:36 -0700

    Working on Question Card Feature

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,29 @@ import { QUESTIONS } from "./data/questions"
 
 function App() {
   const [gameState, setGameState] = useState<GameState>("start")
+  const [selectedAnswer, setSelectedAnswer] = useState<number | null> (null);
+  const [currentQuestion, setCurrentQuestion] = useState<number>(0);
   
   const handleStart = () => {
     setGameState("playing")
   }
 
+  const handleAnswer = (index:number):void => {
+    setSelectedAnswer(index)
+    setCurrentQuestion((prev)=> prev +1)
+  }
+
   return (
-    <div>
+    <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
       {gameState === "start" && <StartScreen onStart = {handleStart}/>}
-      {gameState === "playing" && <QuestionCard question={QUESTIONS[0]} />}
+      {gameState === "playing" && (
+        <div className="p-8">
+        <QuestionCard question={QUESTIONS[currentQuestion]} onAnswerSelect={handleAnswer} selectedAnswer = {selectedAnswer} totalQuestions={QUESTIONS.length} currentQuestion = {currentQuestion}/>
+        </div>)}
       {gameState === "end" && <GameOver/>}
+      
+      </div>
     </div>
    
   )
